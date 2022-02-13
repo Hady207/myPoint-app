23a1ef0484a55fdec2b809df2e1a72be3f710e1e
Hadi <hadimaher207@gmail.com>
2022-02-13 21:32:24 +0300

    logos added

--- app/navigations/tabs/index.tsx
+++ app/navigations/tabs/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Icon} from 'react-native-elements';
 import DataChartsScreen from '@containers/DataCharts';
 import QRScannerScreen from '@containers/QRScanner';
 import BookCalendar from '@containers/BookCalendar';
@@ -10,9 +11,36 @@ const AdminTabs = () => {
   return (
     <Tab.Navigator
       screenOptions={{tabBarHideOnKeyboard: true, headerShown: false}}>
-      <Tab.Screen name="AdminHome" component={DataChartsScreen} />
-      <Tab.Screen name="AdminBarcode" component={QRScannerScreen} />
-      <Tab.Screen name="AdminCalendar" component={BookCalendar} />
+      <Tab.Screen
+        name="AdminHome"
+        component={DataChartsScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: props => (
+            <Icon name="home" type="ant-design" {...props} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="AdminBarcode"
+        component={QRScannerScreen}
+        options={{
+          tabBarLabel: 'Barcode Scanner',
+          tabBarIcon: props => (
+            <Icon name="barcode" type="ant-design" {...props} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="AdminCalendar"
+        component={BookCalendar}
+        options={{
+          tabBarLabel: 'Calendar',
+          tabBarIcon: props => (
+            <Icon name="calendar" type="font-awesome" {...props} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
