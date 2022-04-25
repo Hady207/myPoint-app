c11d9be8e73ffa787d0fb901fb7fd20e544d6f20
Hadi <hadimaher207@gmail.com>
2022-04-25 14:59:10 +0300

    some code cleaning

--- app/navigations/tabs/index.tsx
+++ app/navigations/tabs/index.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import {View} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {Icon} from 'react-native-elements';
 import AnalyticsScreen from '@containers/Analytics';
 import QRScannerScreen from '@containers/QRScanner';
 import BookCalendar from '@containers/BookCalendar';
-import {Colors} from '@styles';
 
 const Tab = createBottomTabNavigator();
 
 const AdminTabs = () => {
-  const insets = useSafeAreaInsets();
   return (
     <Tab.Navigator
       screenOptions={{tabBarHideOnKeyboard: true, headerShown: false}}>
